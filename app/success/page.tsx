0b87b5d7fd69f327181aa2ab3108081f99a7a8e6
linus.schulze <linuscyber146@gmail.com>
2026-01-29 14:34:39 +0100

    sucess

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,27 +1,13 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+
 
 export default function SuccessPage() {
-    const searchParams = useSearchParams();
-    const sessionId = searchParams?.get('session_id');
-    const [orderData, setOrderData] = useState(null);
 
-    useEffect(() => {
-        if (sessionId) {
-            fetch(`/api/checkout-session?session_id=${sessionId}`)
-                .then(res => res.json())
-                .then(data => setOrderData(data));
-        }
-    }, [sessionId]);
-
-    // ...
 
     return (
         <>
-            <div>Zahlung erfolgreich!!</div>
-            <div>Bestellnummer: {sessionId}</div>
+            Success
         </>
 
     )
